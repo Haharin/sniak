3ca6ae5fd492a64a973eba7e39960f4b481ae85a
sniak <ivannikov_ar@mail.ru>
2017-03-06 12:20:24 +0300

    PRIVET

---
+++ test/src/test/java/sda.java
@@ -0,0 +1,9 @@
+/**
+ * Created by sniak on 06.03.2017.
+ */
+public class sda {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+        System.out.println("Hello World!");
+    }
+}
