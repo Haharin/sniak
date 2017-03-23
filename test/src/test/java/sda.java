7e9a1e0db2ecf0c243038d377316e3dac123fad2
sniak <ivannikov_ar@mail.ru>
2017-03-23 20:02:42 +0300

    Я продолжаю удалять

--- test/src/test/java/sda.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sniak on 06.03.2017.
- */
-public class sda {
-    public static void main(String[] args) {
-        // Display "Hello World!"
-        System.out.println("Hello World!");
-    }
-}
